1444c7e4d1501840a6cd6192b528977500c2d167
imran5618 <imran.hossen.5618@gmail.com>
2016-12-18 05:50:25 -0800

    init message

---
+++ app/src/main/java/com/example/lenovo/activitylifecycle/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.lenovo.activitylifecycle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int onCreateCounter;
+    private int onResumeCounter;
+    private int onPauseCounter;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onCreateCounter++;
+        TextView textView=(TextView) findViewById(R.id.onCreate_tv);
+        textView.setTextSize(20);
+        textView.setText("onCreate() called "+""+onCreateCounter);
+        startNewActivity();
+        getTheIntent();
+    }
+    public void getTheIntent()
+    {
+        Intent intent=getIntent();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        onResumeCounter++;
+        TextView textView=(TextView) findViewById(R.id.onResume_tv);
+        textView.setTextSize(20);
+        textView.setText("onResume() called "+""+onResumeCounter);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        onPauseCounter++;
+        TextView textView=(TextView) findViewById(R.id.onPause_tv);
+        textView.setTextSize(20);
+        textView.setText("onPause() called "+""+onPauseCounter);
+    }
+    public void startNewActivity()
+    {
+        button= (Button) findViewById(R.id.button_new);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
